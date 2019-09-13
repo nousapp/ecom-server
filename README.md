cfd35ba09a912b32aa11589a1e4613d0b891dc56
Devon Marantz <devonmarantz@gmail.com>
2019-09-13 14:04:30 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Care Solutions Backend
+
+Backend for Care Solutions app.
+Built with MVC architecture and a file-based DB.
+
+## Setup
+1. Clone
+2. Run `node index.js`
+3. $$$
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const router = require('./middleware/routes');
+const logger = require('./middleware/logger');
+
+const errorHandler = require('./middleware/errorHandler');
+
+const app = express();
+
+// Adds the router middleware
+// app.use(cors());
+// app.use(express.json()); //parse request with JSON bodies
+// app.use(logger);
+// app.use(router);
+// app.use(errorHandler);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Snips server running on port 5000');
+});
