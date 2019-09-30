90928f6538deefcb187f31452ce992812e0326ef
Devon <Devon@domain.local>
2019-09-30 11:56:43 -0700

    Handle query values and intiate request

--- models/Resident.model.js
+++ models/Resident.model.js
@@ -7,13 +7,14 @@ const db = require('mssql');
 
 /**
  * @typedef {Object} Resident
- * @property {string} name
- * @property {string} serviceCode
- * @property {string} servicedBy
- * @property {string} residentID
- * @property {string} roomNum
- * @property {string} date
- */
+ * @property {string} acl
+ * @property {string} LastName
+ * @property {string} FirstName
+ * @property {string} MiddleName
+ * @property {string} SortName
+ * @property {string} Room
+ * @property {string} ResidentId
+ */ 
 
 
 /* Read */
@@ -25,28 +26,75 @@ const db = require('mssql');
  * @returns {Promise<Object[]>}
  */
 // // CODE FOR QUERIES
-// exports.select = async (query = {}) => {
+// exports.select = async ( query = {} ) => {
+//   try {
+//     await db.connect(`${process.env.DATABASE_URL}`);
+//     // CODE FOR QUERIES
+//     const clauses = Object.keys(query)
+//       .map((key,i) => `%s = @${i + 1}`)
+//       .join(' AND ');
+  
+//     const formattedSelect = format(
+//       `SELECT * FROM ${process.env.RESIDENT_DB} ${clauses.length ? `WHERE ${clauses}` : ''}`,
+//       ...Object.keys(query)  
+//     );
+
+//     const result = await db.query(formattedSelect, ...Object.values(query));
+//     // // CODE WITHOUT QUERIES
+//     // const result = await db.query(`SELECT * FROM ${process.env.RESIDENT_DB}`);
+
+//     db.close();
+//     return result.recordset;
+//   } catch (err) {
+//     console.log(err);
+//     if (err instanceof ErrorWithHttpStatus) throw err;
+//     else throw new ErrorWithHttpStatus('Database Error', 500);
+//   }
+// };
+
 exports.select = async ( query = {} ) => {
   try {
-    await db.connect(`${process.env.DATABASE_URL}`);
-    // CODE FOR QUERIES
-    const clauses = Object.keys(query)
-      .map((key,i) => `%I = $${i + 1}`)
-      .join(' AND ');
-    console.log(clauses);
-  
-    const formattedSelect = format(
-      `SELECT * FROM ${process.env.RESIDENT_DB} ${clauses.length ? `WHERE ${clauses}` : ''}`,
-      ...Object.keys(query)
-    );
-    console.log(formattedSelect);
-    console.log(...Object.values(query))
-    const result = await db.query(formattedSelect, Object.values(query));
+    // await db.connect(`${process.env.DATABASE_URL}`);
+    // CODE FOR QUERIES 
+    // const clauses = Object.keys(query)
+    //   .map((key,i) => `%s = ${'${'}valueArray[${i}]}`)
+    //   .join(' AND ');
+    // console.log(`Clauses: ${clauses}`);
+    // console.log(`Object.keys(query): ${Object.keys(query)}`);
+    // console.log(`Object.values(query): ${Object.values(query)}`);
+    // console.log('Format String:');
+    // console.log(`SELECT * FROM ${process.env.RESIDENT_DB} ${clauses.length ? `WHERE ${clauses}` : ''}`);
+    // const formattedSelect = format(
+    //   `SELECT * FROM ${process.env.RESIDENT_DB} ${clauses.length ? `WHERE ${clauses}` : ''}`,
+    //   ...Object.keys(query)  
+    // );
+    // console.log(formattedSelect);
+    // const result = await db.query(formattedSelect, ...Object.values(query));
+
+
+    // MSSQL METHOD
+    // Initiate Request
+    const pool = await db.connect(`${process.env.DATABASE_URL}`);
+    let step1 = await pool.request() 
+    // Handle Queries
+    Object.values(query).forEach(async (value, index) => {
+      step1.input(index, value);
+    })
+    // pool.request().input('1', 'Escueta');
+    // pool.request().input('2', '101');
+    // let result1 = await pool.request()
+    //         .input('1', 'Escueta')
+    //         .input('2', '101')
+    //         .query(`select * from ${process.env.RESIDENT_DB} where LastName = @1 and Room = @2`);
+    // step1.input('0', 'Escueta');
+    let result1 = await step1.query(`select * from ${process.env.RESIDENT_DB} where LastName = @0`);
+
+
     // // CODE WITHOUT QUERIES
     // const result = await db.query(`SELECT * FROM ${process.env.RESIDENT_DB}`);
 
     db.close();
-    return result.recordset;
+    return result1.recordset;
   } catch (err) {
     console.log(err);
     if (err instanceof ErrorWithHttpStatus) throw err;
