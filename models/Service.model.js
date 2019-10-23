cd957596a8757447da5dbdb8e6d3240412dd034b
Devon <devon@nousbiz.com>
2019-10-23 16:07:28 -0700

    Add read function to Service Model

--- models/Service.model.js
+++ models/Service.model.js
@@ -43,7 +43,27 @@ exports.insert = async ({ServiceCode, ServicedBy, TransDate, ResidentId }) => {
 // CODE FOR QUERIES
 exports.select = async ( query = {} ) => {
   try {
-    return 'SELECT SERVICE!';
+    // MSSQL METHOD
+    // Initiate Request
+    const pool = await db.connect(`${process.env.DATABASE_URL}`);
+    let reqPool = await pool.request() 
+    // Handle Query Values
+    Object.values(query).forEach(async (value, index) => {
+      reqPool.input(index, value);
+    })
+    // Handle Query Keys
+    const clauses = Object.keys(query)
+      .map((key,i) => `%I = @${i}`)
+      .join(' AND ');
+    // Handle Format String
+    const formattedSelect = format(
+      `SELECT * FROM ${process.env.SERVICE_DB} ${clauses.length ? `WHERE ${clauses}` : ''}`,
+      ...Object.keys(query)  
+    );
+    // Pass in Query
+    let result = await reqPool.query(formattedSelect);
+    db.close();
+    return result.recordset;
   } catch (err) {
     db.close();
     if (err instanceof ErrorWithHttpStatus) throw err;
