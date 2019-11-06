02f14095f3cecb8a2323d582f1a88a8c041f2510
Devon Marantz <devonmarantz@gmail.com>
2019-11-05 20:50:06 -0800

    Change port to 8080

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.use(express.json()); //parse request with JSON bodies
 app.use(router);
 app.use(errorHandler);
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log('Care Server running on port 5000');
+app.listen(process.env.PORT || 8080, () => {
+  console.log('Care Server running on port 8080');
 });
