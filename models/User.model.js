e35a1515c26e0c77b5b6aaebac57cdba81603675
Devon Marantz <devonmarantz@gmail.com>
2019-11-14 12:40:16 -0800

    Add Read model function

--- models/User.model.js
+++ models/User.model.js
@@ -40,8 +40,29 @@ exports.insert = async ({UserCode, UserName }) => {
 // CODE FOR QUERIES
 exports.select = async ( query = {} ) => {
   try {
-    return 'READ User';
+    // MSSQL METHOD
+    // Initiate Request
+    const pool = await db.connect(`${process.env.DATABASE_URL}`);
+    let reqPool = await pool.request() 
+    // Handle Query Values
+    Object.values(query).forEach(async (value, index) => {
+      reqPool.input(index, value);
+    })
+    // Handle Query Keys
+    const clauses = Object.keys(query)
+      .map((key,i) => `%I = @${i}`)
+      .join(' AND ');
+    // Handle Format String
+    const formattedSelect = format(
+      `SELECT * FROM dbo.users ${clauses.length ? `WHERE ${clauses}` : ''}`,
+      ...Object.keys(query)  
+    );
+    // Pass in Query
+    let result = await reqPool.query(formattedSelect);
+    db.close();
+    return result.recordset;
   } catch (err) {
+    db.close();
     if (err instanceof ErrorWithHttpStatus) throw err;
     else throw new ErrorWithHttpStatus('Database Error', 500);
   }
