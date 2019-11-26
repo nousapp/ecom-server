d7a9e1cd2ee1dab87d95408e39d229a02111651c
Devon Marantz <devonmarantz@gmail.com>
2019-11-26 13:31:13 -0800

    Add storeToken and createUser

--- db/auth.js
+++ db/auth.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const format = require('pg-format');
+const db = require('mssql');
 // Helpers for Authentication
 
 
@@ -31,16 +33,46 @@ async function userExists(username) {
 /**
  * createUser
  * @description Stores user data in the database
- * @param {string} email
+ * @param {string} username
  * @param {string} password
  * @param {string} salt
- * @param {string} fullName
- * @param {string} location
- * @param {string} personaType
+ * @param {string} firstName
+ * @param {string} lastName
+ * @param {string} role
  */
-async function createUser(email, password, salt, fullName, location) {
+async function createUser(Username, Password, Salt, FirstName, LastName, Role) {
   try {
-    // TODO:
+    // Checks if all inputs are in request
+    if(!Username || !Password || !Salt || !FirstName || !LastName || !Role){
+      throw new ErrorWithHttpStatus('Missing Properties', 400);
+    }
+    const pool = await db.connect(`${process.env.DATABASE_URL}`);
+    let idInput = shortid.generate();
+    let dateRequest = await pool.request().query('SELECT getdate();'); 
+    // Destructure date
+    let dateInput =  Object.values(dateRequest.recordset[0])[0];
+    let SortName = `${LastName}, ${FirstName}`;
+
+    // Create Transaction
+    await pool.request()
+      .input('id', db.NVarChar(100), idInput)
+      .input('createTime', dateInput)
+      .input('username', db.NVarChar(100), Username)
+      .input('password', db.NVarChar(100), Password)
+      .input('salt', db.NVarChar(100), Salt)
+      .input('firstName', db.NVarChar(100), FirstName)
+      .input('lastName', db.NVarChar(100), LastName)
+      .input('sortName', db.NVarChar(100), SortName)
+      .input('role', db.NVarChar(100), Role)
+      .query(`INSERT INTO dbo.users (_id, _createdAt, _updatedAt, username, password, salt, FirstName, LastName, SortName, role) VALUES (@id, @createTime, @createTime, @username, @password, @salt, @firstName, @lastName, @sortName, @role);`);
+    
+    // Get created Transaction
+    let result = await pool.request()
+      .input('id', db.NVarChar(100), idInput)
+      .query( `SELECT * FROM dbo.users WHERE _id = @id`);
+    
+    db.close();
+    return result.recordset;
   } catch (err) {
     throw err;
   }
@@ -49,12 +81,34 @@ async function createUser(email, password, salt, fullName, location) {
 /**
  * storeToken
  * @description Stores token in the user's database
- * @param {string} uuid The stored id in the JWT that corresponds with a user.
+ * @param {string} id The stored id in the JWT that corresponds with a user.
  * @param {string} token
  */
-async function storeToken(uuid, token) {
+async function storeToken(id, Token) {
   try {
-    // TODO:
+    const pool = await db.connect(`${process.env.DATABASE_URL}`);
+    // Get Time
+    let dateRequest = await pool.request().query('SELECT getdate();'); 
+    // Destructure date
+    let dateInput =  Object.values(dateRequest.recordset[0])[0];
+
+    // Update Data
+    let reqPool = await pool.request() 
+    // Handle Update Time Input
+    reqPool.input('updateTime', dateInput);
+    reqPool.input('token', Token);
+    reqPool.input('id', db.NVarChar(100), id);
+    var queryText = `UPDATE dbo.transactions SET _updatedAt = @updateTime, token = @Token WHERE _id = @id;`;
+    
+    await reqPool.query(queryText);
+
+    // Get updated Transaction
+    let result = await pool.request()
+      .input('id', db.NVarChar(100), id)
+      .query( `SELECT * FROM dbo.transactions WHERE _id = @id`);
+
+    db.close();
+    return result.recordset;
   } catch (err) {
     throw err;
   }
